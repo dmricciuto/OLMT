ff2b1f56d1318cb67cd3bb58dffb0a75f315d853
Yuan, Fengming <fmyuan@gmail.com>
2020-12-07 22:30:42 -0500

    Add scripts to extract individual surface data, e.g. PCT_PFT, from a user-provided surface data (with one or more gridded variable(s), as specified).

--- daymet_makepointonly_pointlist_example.sh
+++ daymet_makepointonly_pointlist_example.sh
@@ -2,11 +2,12 @@
 
 source ~/.bashrc
 
-ZONING_FILE=zone_mappings.txt
+#ZONING_FILE=zone_mappings.txt
+ZONING_FILE=daymet_elm_mappings.txt 
 
 cwd=$(pwd)
 
-if python ./makepointdata.py \
+if python3 ./makepointdata.py \
   --ccsm_input /Users/f9y/clm4_5_inputdata \
   --keep_duplicates \
   --lat_bounds -999,-999 --lon_bounds -999,-999 \
@@ -14,6 +15,8 @@ if python ./makepointdata.py \
   --model ELM \
   --surfdata_grid --res hcru_hcru \
   --point_list ${ZONING_FILE} \
+  --usersurfnc=/Users/f9y/clm4_5_inputdata/lnd/clm2/surfdata_map/high_res/Tesfa_pnnl_PFT_0.05_MODIS_nwh201201.nc \
+  --usersurfvar='PCT_PFT' \
   --point_area_kmxkm 1.0 & sleep 10
 
 then
