5d52e1b58d5a85d3bed274574608fe52217d64c6
Yuan, Fengming <fmyuan@gmail.com>
2022-08-16 12:03:33 -0400

    Add docker run bash script example for NGEE Arctic PFT option, using Kougarok ngee site data.

---
+++ examples/site_fullrun_docker_userpft_kougarok.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -f
+
+python ./site_fullrun.py \
+      --site AK-KM64 --sitegroup NGEEArctic --caseidprefix TestOMLT \
+      --nyears_ad_spinup 200 --nyears_final_spinup 600 --tstep 1 \
+      --machine docker --compiler gnu --mpilib openmpi \
+      --model_root /E3SM \
+      --ccsm_input /inputdata \
+      --caseroot /output \
+      --surfdata_grid \
+      --spinup_vars \
+      --nopftdyn \
+      --np 2 \
+      --maxpatch_pft 12 \
+      --cpl_bypass --gswp3_daymet4 \
+      --metdir /inputdata/atm/datm7/GSWP3_daymet/cpl_bypass_kougarok \
+      --domainfile /inputdata/share/domains/domain.clm/domain.lnd.51x63pt_kougarok-NGEE_TransA_navy.nc \
+      --surffile /inputdata/lnd/clm2/surfdata_map/surfdata_51x63pt_kougarok-NGEE_TransA_simyr1850_c190604-sub12.nc \
+      --mod_parm_file /inputdata/lnd/clm2/paramdata/clm_params_c180524-sub12.nc
