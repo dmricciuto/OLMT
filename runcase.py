481ccbf99770d5f421e117b5359058a64f128682
Rich Fiorella <rfiorella@lanl.gov>
2022-09-09 09:35:30 -0600

    add ndep scaling options

--- runcase.py
+++ runcase.py
@@ -263,6 +263,10 @@ parser.add_option("--1850_ndep", dest="ndep1850", default=False, \
                   help = 'Use constant 1850 N deposition', action="store_true")
 parser.add_option("--ndep_rcp85", dest="ndeprcp85", default=False, \
                   help = 'Use RCP8.5 N deposition', action="store_true")
+parser.add_option("--scale_ndep", dest="scln", default="", \
+                  help = 'Scaling factor to apply to N deposition in atmospheric forcing')
+parser.add_option("--startdate_scale_ndep", dest="sd_scln", default="99991231", \
+                  help = 'Date (YYYYMMDD) to begin scaling N deposition')                  
 parser.add_option("--1850_aero", dest="aero1850", default=False, \
                   help = 'Use constant 1850 aerosol deposition', action="store_true")
 parser.add_option("--aero_rcp85", dest="aerorcp85", default=False, \
@@ -1460,6 +1464,9 @@ for i in range(1,int(options.ninst)+1):
         else:
           output.write( " stream_fldfilename_ndep = '"+options.ccsm_input+ \
             "/lnd/clm2/ndepdata/fndep_clm_rcp4.5_simyr1849-2106_1.9x2.5_c100428.nc'\n")
+        if (options.scale_ndep != ''):
+          output.write(" scale_ndep = "+str(options.ndep)+"\n")
+          output.write(" startdate_scale_ndep = '"+str(options.sd_ndep)+"'\n")
         if (model_name == 'clm2'):  #set for older tags/branches (option was removed, this may not capture all tags 
                                     #between rename to elm and removal of nitrif_dentrif)   
             output.write(" use_nitrif_denitrif = .true.\n")
