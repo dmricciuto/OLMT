4a2d7075a1747fb8a99505c6b1f7536a13c7f971
Benjamin Sulman <sulmanbn@ornl.gov>
2023-07-14 15:56:14 -0400

    Met data fix

--- runcase.py
+++ runcase.py
@@ -1548,6 +1548,10 @@ for i in range(1,int(options.ninst)+1):
         elif options.metdir != 'none':
             if (options.daymet4 and options.gswp3):
                 output.write(" metdata_type = 'gswp3_daymet4'\n")
+            elif options.daymet and options.gswp3:
+                output.write(" metdata_type = 'gswp3v1_daymet'\n")
+            elif options.gswp3:
+                output.write(" metdata_type = 'gswp3'\n")
             #else:
             #    output.write(" metdata_type = 'gswp3v1_daymet'\n") # This needs to be updated for other types
             output.write(" metdata_bypass = '%s'\n"%options.metdir)
