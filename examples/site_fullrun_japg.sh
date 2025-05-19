19c9a026b278046ac5714db224043cdf58ada5ee
JorgeA-PG <penalozagija@ornl.gov>
2025-05-19 10:36:18 -0400

    removing col3rd and col4th options

--- examples/site_fullrun_japg.sh
+++ examples/site_fullrun_japg.sh
@@ -6,18 +6,21 @@
 MYROOT=/gpfs/wolf2/cades/cli185/
 MYMACH=cades-baseline
 
-nsite_codes="US-GC3,US-GC3,US-GC4"                                      # No space between the variable, the =, and the values inside the " "
+# nsite_codes="US-GC3,US-GC3"                                             # No space between the variable, the =, and the values inside the " "
+nsite_codes="US-GC3,US-GC3,US-GC4"
 
 # lat_coordinates=(38.874076 38.874473 38.874957)                       # [japg] => Chesapeake Bay Coordinates 
-lat_coordinates=(38.874941 38.874941 38.874941)                         # [japg] => From Wetland_sitedata.txt
+lat_coordinates=(38.874941 38.874941 38.874941)                        # [japg] => From Wetland_sitedata.txt
+# lat_coordinates=(38.874941 38.874941)                                   # [japg] => From Wetland_sitedata.txt
 lat_coordinates_str=$(IFS=','; echo "${lat_coordinates[*]}")
 
 # lon_coordinates=(-76.549978 -76.551469 -76.552129)                    # [japg] => Chesapeake Bay Coordinates
-lon_coordinates=(-76.550043 -76.550043 -76.550043)                      # [japg] => From Wetland_sitedata.txt
+# lon_coordinates=(-76.549978 -76.551469)
+lon_coordinates=(-76.550043 -76.550043 -76.550043)                     # [japg] => From Wetland_sitedata.txt
 lon_coordinates_str=$(IFS=','; echo "${lon_coordinates[*]}")
 
 python ./site_fullrun.py \
-      --sitegroup Wetland --caseidprefix C3pt \
+      --sitegroup Wetland --caseidprefix C3v3 \
       --nyears_ad_spinup 20 --nyears_final_spinup 40 --tstep 1 \
       --cpl_bypass --machine $MYMACH --compiler gnu --mpilib openmpi \
       --model_root /ccsopen/home/ji8/E3SM_baseline \
