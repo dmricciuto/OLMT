e920bfb81576cc34e8459074ea8be030b739ec77
Fengming Yuan <fmyuan@gmail.com>
2025-06-19 12:20:12 -0400

    Modify how to run a transient simulation from 'restart' files.

--- examples/2025_Workshop/restart_trans_run.sh
+++ examples/2025_Workshop/restart_trans_run.sh
@@ -35,7 +35,7 @@ done
 case_prefix="${case_prefix:-My}"
 site_name="${site_name:-USARM}"
 transient_years="${transient_years:-10}"
-ncfile_init="${ncfile_init:-/tools/OLMT/examples/full_run_US-ARM_ICB1850CNPRDCTCBC.elm.r.0601-01-01-00000.nc}"
+ncfile_init="${ncfile_init:-/tools/OLMT/examples/2025_Workshop/full_run_US-ARM_ICB1850CNPRDCTCBC.elm.r.0601-01-01-00000.nc}"
 
 
 # =======================================================================================
@@ -66,28 +66,24 @@ if [ ! -d ${ELM_USER_DATA} ]; then
 fi
 cd ${ELM_USER_DATA}
 
-if [ ${ncfile_init} = /tools/OLMT/examples/2025_Workshop/full_run_US-ARM_ICB1850CNPRDCTCBC.elm.r.0601-01-01-00000.nc]; then
-  if [ ! ${site_name} = USARM ] ; then
-     # this file in OLMT/examples/ is specifically for US-ARM only
-     echo " "
-     echo "**** EXECUTION HALTED ****"
-     echo "NO existed initial restart *.elm.r.????-01-01-00000.nc for transient-only run at Site : ${site_name}"
-     exit 0
-     echo " "
-  fi
-fi
+#copy saved resart files: 
+# (1)   # copy 'restart' data packages, usually include: *.elm.r.yyyy-mm-dd-todss.nc, *.cpl.r.yyyy-mm-dd-todss.nc
+#         *.elm.rh0.yyyy-mm-dd-todss.nc, *.elm.h0.yyyy-mm-dd-todss.nc, 
+cp /tools/OLMT/examples/2025_Workshop/full_run_${site_code}*.r.* ${ELM_USER_DATA}/
+cp /tools/OLMT/examples/2025_Workshop/full_run_${site_code}*.rh?.* ${ELM_USER_DATA}/
+# (2) copy 2 (sometime 3) 'rpointer.*' ascii files, in which initial data files will point to.
+cp /tools/OLMT/examples/2025_Workshop/${site_code}_rpointer.lnd ${ELM_USER_DATA}/rpointer.lnd
+cp /tools/OLMT/examples/2025_Workshop/${site_code}_rpointer.drv ${ELM_USER_DATA}/rpointer.drv
+
+# in case we still need 'finidat'
 cp ${ncfile_init} ${ELM_USER_DATA}/full_run_${site_code}_ICB1850CNPRDCTCBC.elm.r.0601-01-01-00000.nc  # assuming the initial restart file are after 600 years spinup.
+
 
 # allow user edit ascii files, which either in ascii or 'ncdump' generated from netcdf files
 if [ ! -f ${ELM_USER_DATA}/user_nl_elm ]; then
   cp /tools/OLMT/examples/user_nl_elm_example user_nl_elm
 fi
-
-#--metdir /inputdata/atm/datm7/atm_forcing.datm7.GSWP3.0.5d.v2.c180716_Workshop/cpl_bypass_${site_code} \
-#      --domainfile /inputdata/share/domains/domain.clm/domain_${site_code}_simyr1850_c170912.nc \
-#      --surffile /inputdata/lnd/clm2/surfdata_map/surfdata_${site_code}_simyr1850_c170912.nc \
  
-
 if [ ! -f ${ELM_USER_DATA}/domain.txt ]; then
   ncdump /inputdata/share/domains/domain.clm/domain_${site_code}_simyr1850_c170912.nc>&domain.txt
 fi
@@ -98,16 +94,20 @@ if [ ! -f ${ELM_USER_DATA}/surfdata.txt ]; then
 fi
 ncgen -o surfdata.nc surfdata.txt
 
-if [ ! -f ${ELM_USER_DATA}/surfdata.pftdyn.txt ]; then
-  ncdump /inputdata/lnd/clm2/surfdata_map/landuse.timeseries_${site_code}_simyr1850_c170912.nc>&surfdata.pftdyn.txt
-fi
-ncgen -o surfdata.pftdyn.nc surfdata.pftdyn.txt
-
+#if [ ! -f ${ELM_USER_DATA}/surfdata.pftdyn.txt ]; then
+#  ncdump /inputdata/lnd/clm2/surfdata_map/landuse.timeseries_${site_code}_simyr1850_c170912.nc>&surfdata.pftdyn.txt
+#fi
+#ncgen -o surfdata.pftdyn.nc surfdata.pftdyn.txt
 
 if [ ! -f ${ELM_USER_DATA}/clm_params.txt ]; then
   ncdump /inputdata/lnd/clm2/paramdata/clm_params_c180524.nc>&clm_params.txt
 fi
 ncgen -o clm_params.nc clm_params.txt
+
+if [ ! -f ${ELM_USER_DATA}/CNP_parameters.txt ]; then
+  ncdump /inputdata/lnd/clm2/paramdata/CNP_parameters_c180529.nc>&CNP_parameters.txt
+fi
+ncgen -o CNP_parameters.nc CNP_parameters.txt
 
 chmod 777 *.txt
 chmod 777 *.nc
@@ -116,6 +116,7 @@ chmod 777 user_nl_elm
 #
 cd /tools/OLMT
 
+# setup/build transient case using OLMT, but NO run (--no_submit)
 if python3 ./site_fullrun.py \
       --site ${site_code} --sitegroup Workshop --caseidprefix ${case_prefix} \
       --noad --nofnsp --nyears_transient ${transient_years} --tstep 1 \
@@ -127,14 +128,26 @@ if python3 ./site_fullrun.py \
       --runroot /output \
       --nopointdata \
       --metdir /inputdata/atm/datm7/atm_forcing.datm7.GSWP3.0.5d.v2.c180716_Workshop/cpl_bypass_${site_code} \
-      --mod_parm_file $ELM_USER_DATA/clm_params.nc \
       --namelist_file $ELM_USER_DATA/user_nl_elm \
       --domainfile $ELM_USER_DATA/domain.nc \
       --surffile $ELM_USER_DATA/surfdata.nc \
       --nopftdyn \
-      --finitfile $ELM_USER_DATA/full_run_${site_code}_ICB1850CNPRDCTCBC.elm.r.0601-01-01-00000.nc \
+      --no_submit \
       & sleep 10
 
+# the following line is needed to insert into above, if run normally
+#      --finitfile $ELM_USER_DATA/full_run_${site_code}_ICB1850CNPRDCTCBC.elm.r.0601-01-01-00000.nc \
+      
+  # access CASE directory to copy 2 parameter files.
+  cd /output/cime_case_dirs/${case_prefix}_${site_code}_ICB20TRCNPRDCTCBC
+
+  # OLMT options '--mod_param_file' not works in this setup (TODO checking).
+  cp $ELM_USER_DATA/clm_params.nc \
+     /output/cime_run_dirs/${case_prefix}_${site_code}_ICB20TRCNPRDCTCBC/run
+
+  cp $ELM_USER_DATA/CNP_parameters.nc \
+     /output/cime_run_dirs/${case_prefix}_${site_code}_ICB20TRCNPRDCTCBC/run
+    
 then
   wait
 
@@ -143,6 +156,19 @@ then
 else
   exit &?
 fi
+
+# Above OLMT call actually set up a TR20 case, based on its final spinup case
+# we need to change that   
+cd /output/cime_case_dirs/${case_prefix}_${site_code}_ICB20TRCNPRDCTCBC
+./xmlchange RUN_TYPE=branch 
+./xmlchange RUN_REFDIR=$ELM_USER_DATA
+./xmlchange RUN_REFCASE=full_run_${site_code}_ICB20TRCNPRDCTCBC
+./xmlchange RUN_REFDATE=2000-01-01
+./xmlchange GET_REFCASE=TRUE
+
+# submit model run
+./case.submit
+
 
 #
 cd /output/cime_run_dirs/${case_prefix}_${site_code}_ICB20TRCNPRDCTCBC/run
