628dda440999f61114e8c9d12f4b14a2116093f1
Rich Fiorella <rfiorella@lanl.gov>
2025-07-08 12:57:59 -0600

    output->option typo

--- site_fullrun.py
+++ site_fullrun.py
@@ -263,7 +263,7 @@ parser.add_option("--use_arctic_init", dest="use_arctic_init", default=False, \
 parser.add_option("--use_IM2_hillslope_hydrology", dest="use_IM2_hillslope_hydrology", default=False, \
                   help="Use IM2 hillslope hydrology parameterization", action="store_true")
 # adjust topounit/pft output:
-parser.add_output("--arctic_topounit_output", dest="arctic_topounit_output",default=False, \
+parser.add_option("--arctic_topounit_output", dest="arctic_topounit_output",default=False, \
                   help="Activate topounit-level and pft-level outputs by turning on hist_dov2xy")
 
 #datasets for user-defined PFTs (by F-M Yuan, NGEE-Arctic)
